6d5a613a7fec412dbda07301bd6f7e80ff4a37cc
Chitroopa Manikkavasagam <chitroopa72193@gmail.com>
2017-07-26 15:53:30 -0700

    Add account history

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,13 +1,6 @@
 class AccountsController < ApplicationController
-  def index
-  end
-
-  def new
-  end
-
-  def edit
-  end
-
   def show
+    @account = Account.find_by(user_id: current_user.id)
+    @account_history = @account.orders.where(status: "Paid")
   end
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,11 +1,15 @@
 class Order < ApplicationRecord
   belongs_to :account
   has_many :order_items
-  before_save :update_total
+  before_save :update_total_and_items
   before_create :update_status
 
   def calculate_total
     self.order_items.collect { |item| item.product.price * item.quantity }.sum
+  end
+
+  def calculate_no_of_items
+    self.order_items.collect { |item| item.quantity }.sum
   end
 
   private
@@ -16,7 +20,9 @@ class Order < ApplicationRecord
     end
   end
 
-  def update_total
+  def update_total_and_items
     self.total_price = calculate_total
+    self.no_of_items = calculate_no_of_items
   end
+
 end
